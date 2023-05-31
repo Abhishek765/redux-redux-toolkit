642ef559e1b0fc8e86955835998cbce319d7280f
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-06-01 00:42:02 +0530

    extraReducers for ice-cream slice

--- redux-toolkit/features/icecream/icecreamSlice.js
+++ redux-toolkit/features/icecream/icecreamSlice.js
@@ -1,4 +1,5 @@
 const { createSlice } = require("@reduxjs/toolkit");
+const { cakeActions } = require("../cake/cakeSlice");
 
 const initialState = {
   numOfIceCreams: 20,
@@ -14,6 +15,12 @@ const iceCreamSlice = createSlice({
     restocked: (state, action) => {
       state.numOfIceCreams += action.payload;
     },
+  },
+  // Creating a scenario to decrement the number of ice-cream when a  cake is ordered
+  extraReducers: (builder) => {
+    builder.addCase(cakeActions.ordered, (state) => {
+      state.numOfIceCreams--;
+    });
   },
 });
 
