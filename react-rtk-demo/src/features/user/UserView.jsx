075d4a8f82758ead80228c85739e0d051c5f28f9
Abhishek765 <vishwakarmaabhishek31@gmail.com>
2023-06-04 18:01:57 +0530

    dispatching users list

--- react-rtk-demo/src/features/user/UserView.jsx
+++ react-rtk-demo/src/features/user/UserView.jsx
@@ -1,7 +1,30 @@
+import { useEffect } from 'react';
+import { fetchUsers } from './userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 const UserView = () => {
+  const dispatch = useDispatch();
+  const usersDetail = useSelector((state) => state.user);
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, []);
+
   return (
     <div>
       <h2>List of Users</h2>
+      {usersDetail?.loading && <p>Loading...</p>}
+      {!usersDetail?.loading && usersDetail?.error && (
+        <p>Error: {usersDetail.error}</p>
+      )}
+      {!usersDetail?.loading && usersDetail?.users.length > 0 ? (
+        <ul>
+          {usersDetail.users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- react-rtk-demo/src/features/user/userSlice.js
+++ react-rtk-demo/src/features/user/userSlice.js
@@ -1,21 +1,21 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
   loading: false,
   users: [],
-  error: "",
+  error: ''
 };
 
 // This thunk will generate actions types pending, fulfilled, and rejected
-export const fetchUsers = createAsyncThunk("user/fetchedUsers", () => {
+export const fetchUsers = createAsyncThunk('user/fetchedUsers', () => {
   return axios
-    .get("https://jsonplaceholder.typicode.com/users")
-    .then((response) => response.data.map((user) => user.id));
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.data);
 });
 
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   extraReducers: (builder) => {
     builder.addCase(fetchUsers.pending, (state) => {
@@ -25,7 +25,7 @@ const userSlice = createSlice({
     builder.addCase(fetchUsers.fulfilled, (state, action) => {
       state.users = action.payload;
       state.loading = false;
-      state.error = "";
+      state.error = '';
     });
 
     builder.addCase(fetchUsers.rejected, (state, action) => {
@@ -33,7 +33,7 @@ const userSlice = createSlice({
       state.users = [];
       state.loading = false;
     });
-  },
+  }
 });
 
 export default userSlice.reducer;
